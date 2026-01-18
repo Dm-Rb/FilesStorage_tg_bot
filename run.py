de4816213540a558cfed4894be9a9fe7f6a40b07
Dm-Rb <dm.rb.418http@gmail.com>
2026-01-19 02:40:14 +0300

    run.py update

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from folder_watcher import run_watcher
 from telegram_bot.bot import start_bot_wrapper
 
 if __name__ == "__main__":
-    multiprocessing.set_start_method("spawn", force=True)
+    multiprocessing.set_start_method("fork", force=True)
     new_folders_queue = multiprocessing.Queue()
     process_monitoring = multiprocessing.Process(target=run_watcher, args=(new_folders_queue,))
     process_bot = multiprocessing.Process(target=start_bot_wrapper, args=(new_folders_queue,))
